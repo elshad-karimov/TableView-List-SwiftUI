e1e4b657a19d3f78cfd410944c75cfc948575812
Edy <elshad040@gmail.com>
2019-06-28 11:52:54 +0400

    Initial Commit

---
+++ TableView (List) SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TableView (List) SwiftUI
+//
+//  Created by Edy on 6/28/19.
+//  Copyright © 2019 AppMillers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
