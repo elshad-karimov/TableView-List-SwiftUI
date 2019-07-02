ba3099f5017c1b63918fb256a6a476dbb77b0534
Edy <elshad040@gmail.com>
2019-07-02 21:59:59 +0400

    How to create table view in SwiftUI

--- TableView (List) SwiftUI/ContentView.swift
+++ TableView (List) SwiftUI/ContentView.swift
@@ -9,8 +9,20 @@
 import SwiftUI
 
 struct ContentView : View {
+    var titles = ["Swift", "SwiftUI", "Xcode", "iOS", "Mac"]
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(0..<titles.count) { index in
+                HStack {
+                    Image(self.titles[index]).resizable()
+                        .frame(width: 60, height: 60)
+                    Text(self.titles[index])
+                }
+            }
+            .navigationBarTitle(Text("Apple List"), displayMode: .large)
+        }
+  
     }
 }
 
